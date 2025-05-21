8ff65d28adf65172f9ef0701e6128bbcdfe5e18c
Yugoslife <vasetskiy.y@gmail.com>
2025-05-21 12:30:46 +0200

    ci: add retries to Cypress and fix CI pipeline

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,18 @@
-// cypress.config.ts
-import { defineConfig } from 'cypress';
+import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
-    // Путь указывает на ваш локальный Vite-сервер
     baseUrl: 'http://localhost:5173',
+    viewportWidth: 1280,
+    viewportHeight: 800,
+    defaultCommandTimeout: 10000,
+    pageLoadTimeout: 60000,
     specPattern: 'cypress/e2e/**/*.cy.ts',
     supportFile: 'cypress/support/e2e.ts',
+    env: { apiUrl: 'http://localhost:4000' },
+    retries: {
+      runMode: 2,
+      openMode: 0,
+    },
   },
-});
+})
